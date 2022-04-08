f6d3cf56579d405454b9b461883a374cc91eade6
禺狨 <18119604035@163.com>
2022-04-08 10:31:08 +0800

    qq快捷登录失败地址修复

--- src/components/logindialog.vue
+++ src/components/logindialog.vue
@@ -920,7 +920,7 @@ export default {
         "/api/v2/login/socialite/qq?success_redirect=" +
         urlencode(successRedirectUrl) +
         "&failed_redirect=" +
-        urlencode(this.config.url + "/500");
+        urlencode(this.$utils.getAppUrl() + "/500");
     },
     changeWeixin() {
       this.dialogStatus = 5;
