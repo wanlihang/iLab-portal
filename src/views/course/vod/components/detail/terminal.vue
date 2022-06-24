32f31ed821f8c4d9c559226775ca840d88eae8a7
wanlihang <lihang_wan@outlook.com>
2022-06-24 15:06:55 +0800

    feat: 命令行容器直连demo

---
+++ src/views/course/vod/components/detail/terminal.vue
@@ -0,0 +1,35 @@
+<template>
+  <div>
+    <el-container>
+      <el-aside width="30%" style="background-color: rgb(238, 241, 246)">
+        <iframe :src="src1" ref="iframe1" height="500px" width="100%"></iframe>
+      </el-aside>
+      <el-aside width="70%" style="background-color: rgb(238, 241, 211)">
+        <iframe :src="src2" ref="iframe2" height="500px" width="100%"></iframe>
+      </el-aside>
+    </el-container>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'Terminal',
+  data() {
+    return {
+      containerId: 'f68b3a91e526', //this.$route.params.containerId
+      src1: 'http://localhost:8000/',
+      // 连接容器的地址
+      src2: 'http://192.168.102.40:9000/?arg=test&arg=python-5f584b798c-6trkx&arg=%2fbin%2fbash'
+    }
+  },
+  mounted() {
+
+  },
+  beforeDestroy() {
+
+  }
+}
+</script>
+<style scoped>
+</style>
--- src/views/course/vod/components/detail/video-chapter-list.vue
+++ src/views/course/vod/components/detail/video-chapter-list.vue
@@ -4,34 +4,34 @@
       <div class="chapter-name">{{ chapter.title }}</div>
       <div class="chapter-videos-box">
         <div
-          class="video-item"
-          @click="switchVideo(videoItem)"
-          v-for="videoItem in videos[chapter.id]"
-          :key="videoItem.id"
+            class="video-item"
+            @click="switchVideo(videoItem)"
+            v-for="videoItem in videos[chapter.id]"
+            :key="videoItem.id"
         >
           <img
-            class="play-icon"
-            v-if="
+              class="play-icon"
+              v-if="
               isBuy ||
                 course.is_free === 1 ||
                 videoItem.charge === 0 ||
                 (videoItem.charge > 0 && videoItem.free_seconds > 0) ||
                 buyVideos.indexOf(videoItem.id) !== -1
             "
-            src="@/assets/img/commen/icon-unlock.png"
+              src="@/assets/img/commen/icon-unlock.png"
           />
           <img
-            class="play-icon"
-            v-else
-            src="@/assets/img/commen/icon-lock.png"
+              class="play-icon"
+              v-else
+              src="@/assets/img/commen/icon-lock.png"
           />
           <div class="video-title">
             <div class="text">
               {{ videoItem.title }}
             </div>
             <div
-              class="free"
-              v-if="
+                class="free"
+                v-if="
                 isBuy === false &&
                   course.is_free !== 1 &&
                   (videoItem.charge === 0 || videoItem.free_seconds > 0)
@@ -44,6 +44,20 @@
             <duration :seconds="videoItem.duration"></duration>
           </div>
         </div>
+        <div style="margin-left: 35px">
+          <el-button type="primary" size="small" @click="openWebSSH">
+            开启实验<i class="el-icon-upload el-icon--right"></i>
+          </el-button>
+        </div>
+        <el-dialog
+            :title="title"
+            :visible.sync="open"
+            width="80%"
+            center>
+          <span slot="footer" class="dialog-footer">
+          <Terminal></Terminal>
+        </span>
+        </el-dialog>
       </div>
     </div>
     <template v-if="videos[0] && videos[0].length > 0">
@@ -51,34 +65,34 @@
         <div class="chapter-name">无章节内容</div>
         <div class="chapter-videos-box">
           <div
-            class="video-item"
-            @click="switchVideo(videoItem)"
-            v-for="videoItem in videos[0]"
-            :key="videoItem.id"
+              class="video-item"
+              @click="switchVideo(videoItem)"
+              v-for="videoItem in videos[0]"
+              :key="videoItem.id"
           >
             <img
-              class="play-icon"
-              v-if="
+                class="play-icon"
+                v-if="
                 isBuy ||
                   course.is_free === 1 ||
                   videoItem.charge === 0 ||
                   (videoItem.charge > 0 && videoItem.free_seconds > 0) ||
                   buyVideos.indexOf(videoItem.id) !== -1
               "
-              src="@/assets/img/commen/icon-unlock.png"
+                src="@/assets/img/commen/icon-unlock.png"
             />
             <img
-              class="play-icon"
-              v-else
-              src="@/assets/img/commen/icon-lock.png"
+                class="play-icon"
+                v-else
+                src="@/assets/img/commen/icon-lock.png"
             />
             <div class="video-title">
               <div class="text">
                 {{ videoItem.title }}
               </div>
               <div
-                class="free"
-                v-if="
+                  class="free"
+                  v-if="
                   isBuy === false &&
                     course.is_free !== 1 &&
                     (videoItem.charge === 0 || videoItem.free_seconds > 0)
@@ -99,14 +113,28 @@
 
 <script>
 import Duration from "@/components/duration.vue";
+import Terminal from './terminal'
+
 export default {
   components: {
     Duration,
+    Terminal
   },
   props: ["videos", "isBuy", "course", "buyVideos", "chapters"],
+  data() {
+    return {
+      open: false,
+      title: ""
+    };
+  },
   methods: {
     switchVideo(item) {
       this.$emit("switchVideo", item);
+    },
+    /** 打开实验环境按钮操作 */
+    openWebSSH() {
+      this.open = true;
+      this.title = "命令行";
     },
   },
 };
@@ -122,6 +150,7 @@ export default {
   position: relative;
   margin-top: 30px;
   cursor: pointer;
+
   &:first-child {
     margin-top: 0px;
   }
@@ -131,6 +160,7 @@ export default {
     height: 24px;
     cursor: pointer;
   }
+
   .video-title {
     height: 22px;
     font-size: 14px;
@@ -141,6 +171,7 @@ export default {
     display: flex;
     flex-direction: row;
     align-items: center;
+
     .free {
       margin-left: 15px;
       width: 44px;
@@ -155,6 +186,7 @@ export default {
       justify-content: center;
     }
   }
+
   .video-duration {
     position: absolute;
     height: 14px;
@@ -166,13 +198,16 @@ export default {
     right: 0;
   }
 }
+
 .chapter-item {
   width: 100%;
   height: auto;
   margin-top: 50px;
+
   &:first-child {
     margin-top: 0px;
   }
+
   .chapter-name {
     width: 100%;
     height: 16px;
@@ -181,16 +216,20 @@ export default {
     color: #333333;
     line-height: 16px;
     margin-bottom: 30px;
+
     &:first-child {
       margin-bottom: 0px;
     }
+
     &:last-child {
       margin-bottom: 0px;
     }
   }
+
   .chapter-videos-box {
     width: 100%;
     height: auto;
+
     .video-item {
       width: 100%;
       height: 24px;
@@ -206,6 +245,7 @@ export default {
         height: 24px;
         cursor: pointer;
       }
+
       .video-title {
         height: 22px;
         font-size: 14px;
@@ -216,6 +256,7 @@ export default {
         display: flex;
         flex-direction: row;
         align-items: center;
+
         .free {
           margin-left: 15px;
           width: 44px;
@@ -230,6 +271,7 @@ export default {
           justify-content: center;
         }
       }
+
       .video-info {
         position: absolute;
         height: 14px;
